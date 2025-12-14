e50db41c494a1473f002ada141cb3271638dd036
DigitalKevin <kevinhoang670@gmail.com>
2025-12-14 00:57:34 -0800

    Attempt 6 to fix vercel bug

--- app/api/groups/route.js
+++ app/api/groups/route.js
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma';
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export async function POST(req) {
   try {
